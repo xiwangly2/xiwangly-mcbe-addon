a6d1af6f60f66b4fc0d8130c6a0a04fc010117c0
xiwangly2 <1334850101@qq.com>
2023-01-14 10:57:50 +0800

    提交

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@
 chcp 65001
 del "Xiwangly的复合资源包.mcaddon"
 ".\7z2201-extra\7za.exe" a -tzip -mx0 -x@tar_exclude.txt "Xiwangly的复合资源包.mcaddon" .
-@REM start "" "C:\Program Files\Bandizip\Bandizip.exe" "Xiwangly的复合资源包.mcaddon"
-start "Minecraft.lnk" "Xiwangly的复合资源包.mcaddon"
+start "" "C:\Program Files\Bandizip\Bandizip.exe" "Xiwangly的复合资源包.mcaddon"
+@REM start "Minecraft.lnk" "Xiwangly的复合资源包.mcaddon"
