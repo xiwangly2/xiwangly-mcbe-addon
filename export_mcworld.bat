a026ae0d584609d78a59ba67e950d17e17272ed1
xiwangly2 <1334850101@qq.com>
2023-01-15 12:28:03 +0800

    更改路径

--- export_mcworld.bat
+++ export_mcworld.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
 del "Xiwangly的生存.mcworld"
-".\7z2201-extra\7za.exe" a -tzip -mx0 "Xiwangly的生存.mcworld" "C:/Users/Xiwangly/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/-YemYwArJwA=/"
+".\7z2201-extra\7za.exe" a -tzip -mx0 "Xiwangly的生存.mcworld" "C:/Users/Xiwangly/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/-YemYwArJwA=/*"
 start "" "C:\Program Files\Bandizip\Bandizip.exe" "Xiwangly的生存.mcworld"
