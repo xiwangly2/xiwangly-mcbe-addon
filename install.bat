ab11294257a235df02e34d3ac6ae57a099195e0d
xiwangly2 <1334850101@qq.com>
2023-01-15 20:58:38 +0800

    提交

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@
 chcp 65001
 set file_name="Xiwangly的复合资源包.mcaddon"
 del %file_name%
-".\7z2201-extra\7za.exe" a -tzip -mx0 -x@tar_exclude.txt %file_name% .
+".\7z2201-extra\7za.exe" a -tzip -mx0 -x@zip_exclude.txt %file_name% .
 @REM start "" "C:\Program Files\Bandizip\Bandizip.exe" %file_name%
 start "Minecraft.lnk" %file_name%
