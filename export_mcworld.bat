f1b14ed9feb66869abf8c1a7ca042662d1f08f02
xiwangly2 <1334850101@qq.com>
2023-01-21 12:06:28 +0800

    提及

--- export_mcworld.bat
+++ export_mcworld.bat
@@ -1,6 +1,11 @@
 @echo off
 chcp 65001
-set file_name="Xiwangly的生存.mcworld"
-del %file_name%
-".\7z2201-extra\7za.exe" a -tzip -mx0 %file_name% "C:/Users/Xiwangly/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/-YemYwArJwA=/*"
-start "" "C:\Program Files\Bandizip\Bandizip.exe" %file_name%
+cd "backup"
+set file_date=%date:~3,4%%date:~8,2%%date:~11,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+set file_name="Xiwangly的生存_%file_date%.mcworld"
+goto export_mcworld
+
+:export_mcworld
+@REM del %file_name%
+"../7z2201-extra/7za.exe" a -tzip -mx0 %file_name% "C:/Users/Xiwangly/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/-YemYwArJwA=/*"
+start "" "C:/Program Files/Bandizip/Bandizip.exe" %file_name%
