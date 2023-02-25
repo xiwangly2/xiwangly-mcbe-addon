7c49f886fcade0cbfdf0e9636fd1593b1f7307c1
xiwangly2 <1334850101@qq.com>
2023-02-25 10:53:16 +0800

    1

--- export_mcworld.bat
+++ export_mcworld.bat
@@ -3,7 +3,7 @@ chcp 65001
 cd "backup"
 set file_date=%date:~3,4%%date:~8,2%%date:~11,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 set file_name="Xiwangly的生存_%file_date%.mcworld"
-set world=xo3wY21NHwA=
+set world=I3z4Y912OAA=
 set world_dir="C:/Users/Xiwangly/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/%world%/*"
 goto export_mcworld
 
